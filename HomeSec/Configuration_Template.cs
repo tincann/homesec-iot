3b1b03eb593b61decf90e4754c7c85240b764039
tincann <saabnl@gmail.com>
2016-05-08 17:13:24 +0200

    Added configuration template

---
+++ HomeSec/Configuration_Template.cs
@@ -0,0 +1,18 @@
+﻿namespace HomeSec
+{
+    //Rename this file and class to Configuration
+    public static class Configuration_Template
+    {
+        //GPIO BCM Pin number for the door sensor
+        public const int DoorSensorPin = 17;
+
+        //Telegram Bot Token
+        public const string TelegramApiToken = "";
+        
+        //Telegram chat ids that can send commands through the bot
+        public static int[] CommandChatIdWhitelist = { };
+
+        //Telgram chat ids of chats that will be notified when the notifier is triggered
+        public static int[] NotifyChatIds = { };
+    }
+}
